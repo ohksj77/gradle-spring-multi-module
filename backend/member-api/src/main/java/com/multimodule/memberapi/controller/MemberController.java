a5e05fc338ba0de6eda499166d1f098249c7fcf5
김승진 <ohksj77@gmail.com>
2024-01-14 18:45:32 +0900

    feat: add http status

--- backend/member-api/src/main/java/com/multimodule/memberapi/controller/MemberController.java
+++ backend/member-api/src/main/java/com/multimodule/memberapi/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.multimodule.memberapi.controller;
 import com.multimodule.core.dto.IdResponse;
 import com.multimodule.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import com.multimodule.member.dto.MemberRequest;
 import com.multimodule.member.dto.MemberResponse;
@@ -18,11 +19,13 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public IdResponse<UUID> create(@RequestBody final MemberRequest memberRequest) {
         return memberService.createMember(memberRequest);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<MemberResponse> getAll() {
         return memberService.getAll();
     }
